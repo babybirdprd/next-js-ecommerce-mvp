42ff5735f6dec76c6454041d654dba7ff2c2e101
babybirdprd <89063959+babybirdprd@users.noreply.github.com>
2024-07-03 03:18:54 -0400

    Create page.tsx

---
+++ src/app/admin/stores/new/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "../../_components/PageHeader"
+import { StoreForm } from "../_components/StoreForm"
+
+export default function NewStorePage() {
+  return (
+    <>
+      <PageHeader>Add Store</PageHeader>
+      <StoreForm />
+    </>
+  )
+}
